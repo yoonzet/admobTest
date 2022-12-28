8cfa44b19e05f7010fce1af6d4119abe1531b599
yoonji lee <lalala703@naver.com>
2022-12-28 12:16:50 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
+
+const adUnitId = __DEV__
+  ? TestIds.BANNER
+  : 'ca-app-pub-3940256099942544/6300978111';
+
+function App() {
+  return (
+    <>
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.FULL_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+      />
+    </>
+  );
+}
+export default App;
